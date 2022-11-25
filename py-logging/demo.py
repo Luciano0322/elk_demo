43d93620554de95e8af81064077e882984cb999b
Luciano Lee <luciano.lee@webcomm.com.tw>
2022-11-25 15:36:04 +0800

    change port in py

--- py-logging/demo.py
+++ py-logging/demo.py
@@ -7,7 +7,7 @@ host = 'localhost'
 test_logger = logging.getLogger('python-logstash-logger')
 test_logger.setLevel(logging.INFO)
 # test_logger.addHandler(logstash.LogstashHandler(host, 50000, version=1))
-test_logger.addHandler(logstash.TCPLogstashHandler(host, 5959, version=1))
+test_logger.addHandler(logstash.TCPLogstashHandler(host, 50000, version=1))
 
 test_logger.error('python-logstash: test logstash error message.')
 test_logger.info('python-logstash: test logstash info message.')
